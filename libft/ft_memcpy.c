9d82db380053350acee624fdc63ad9a0fb7fa1bf
U-lo-l <jeffk0408@naver.com>
2021-11-29 15:43:34 +0900

    ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 03:50:49 by dkim2             #+#    #+#             */
-/*   Updated: 2021/11/29 15:30:57 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/11/29 15:42:48 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (dest == NULL)
+		return (NULL);
 	while (i < n)
 	{
 		((t_uchar *)dest)[i] = ((const t_uchar *)src)[i];
