e47d1cb6c6d417dfc38f7ade0dbb377775e786ce
U-lo-l <jeffk0408@naver.com>
2021-11-29 16:05:32 +0900

    ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,33 +6,13 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 03:50:49 by dkim2             #+#    #+#             */
-/*   Updated: 2021/11/29 16:01:42 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/11/29 16:04:10 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 typedef unsigned char	t_uchar;
-/*
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	char	*dest2;
-	char	*src2;
-	size_t	i;
-
-	dest2 = dest;
-	src2 = (char *)src;
-	i = 0;
-	if (dest2 == src2)
-		return (dest2);
-	while (i < n)
-	{
-		dest2[i] = src2[i];
-		i++;
-	}
-	return (dest);
-}
-*/
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
