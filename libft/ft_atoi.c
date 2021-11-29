3b1c078248226a12c14782b3042de93e39d80a2e
U-lo-l <jeffk0408@naver.com>
2021-11-29 17:04:07 +0900

    ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 23:38:15 by dkim2             #+#    #+#             */
-/*   Updated: 2021/11/29 17:03:03 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/11/29 17:04:03 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ static int	ft_is_space(char c)
 
 int	ft_atoi(const char *nptr)
 {
-	unsigned int	nbr;
-	int				sign;
+	int	nbr;
+	int	sign;
 
 	sign = 1;
 	nbr = 0;
