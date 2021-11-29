2d54804262985b4863b08b7f539369024c2e7114
U-lo-l <jeffk0408@naver.com>
2021-11-29 17:47:00 +0900

    ft_substr.c

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 13:34:29 by dkim2             #+#    #+#             */
-/*   Updated: 2021/11/29 17:44:55 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/11/29 17:46:50 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,3 +34,28 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	substr[i] = 0;
 	return (substr);
 }
+/* [BAD CODE]
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+	size_t	size;
+
+	if (!s)
+		return (NULL);
+	size = ft_strlen(s);
+	if (start >= size)
+		return ("");
+	substr = malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);	
+	i = 0;
+	while (i < len && start + i < size)
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = 0;
+	return (substr);
+}
+*/
