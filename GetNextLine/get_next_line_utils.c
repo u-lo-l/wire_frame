2f5adf3e4bd4f565d3359bd4d7e699c7c5748e1f
dkim2 <jeffk0408@naver.com>
2021-12-02 18:03:52 +0900

    get_next_line_utils.c : gnl_stralloc add

--- GetNextLine/get_next_line_utils.c
+++ GetNextLine/get_next_line_utils.c
@@ -6,11 +6,25 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 12:57:01 by dkim2             #+#    #+#             */
-/*   Updated: 2021/12/02 17:44:41 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/02 18:03:30 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*gnl_stralloc(int strlen)
+{
+	char	*new_str;
+	int		i;
+
+	new_str = malloc(sizeof(char) * (strlen + 1));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (i <= strlen)
+		new_str[i++] = 0;
+	return (new_str);
+}
 
 char	*gnl_strchr(char *str, char c)
 {
