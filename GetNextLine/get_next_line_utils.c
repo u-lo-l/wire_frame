442709e7a7484369aa5f6655fc0053ba68c59d79
dkim2 <jeffk0408@naver.com>
2021-12-02 18:25:17 +0900

    get_next_line_utils.c : gnl_strdup add

--- GetNextLine/get_next_line_utils.c
+++ GetNextLine/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 12:57:01 by dkim2             #+#    #+#             */
-/*   Updated: 2021/12/02 18:12:16 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/02 18:24:55 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,19 @@ char	*gnl_strchr(char *str, char c)
 		return (str);
 	else
 		return (NULL);
+}
+
+char	*gnl_strdup(char *str)
+{
+	int		size;
+	char	*new_str;
+	
+	size = 0;
+	while (str[size])
+		size++;
+	new_str = gnl_stralloc(size + 1);
+	size = -1;
+	while (str[++size])
+		new_str[size] = str[size];
+	return (new_str);
 }
