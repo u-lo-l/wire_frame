6a7bcc3ffed0f7143043b7f2988f2570bc7ea1a5
dkim2 <jeffk0408@naver.com>
2021-12-02 17:48:15 +0900

    get_next_line_utils.c : gnl_strchr added

--- GetNextLine/get_next_line_utils.c
+++ GetNextLine/get_next_line_utils.c
@@ -6,8 +6,24 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 12:57:01 by dkim2             #+#    #+#             */
-/*   Updated: 2021/12/01 13:00:18 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/02 17:44:41 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*gnl_strchr(char *str, char c)
+{
+	int	str_end;
+
+	str_end = 0;
+	while(!str_end)
+	{
+		if (!*str)
+			str_end = 1;
+		if (*str == c)
+			return (str);
+		str++;
+	}
+	return (NULL);
+}
