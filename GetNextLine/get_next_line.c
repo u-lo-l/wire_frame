357bac4928eef04a60cd5111b15ff2895e6f590c
U-lo-l <jeffk0408@naver.com>
2021-12-03 08:21:56 +0900

    get_next_line save

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 12:56:54 by dkim2             #+#    #+#             */
-/*   Updated: 2021/12/02 20:43:10 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/03 06:01:16 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 #include <unistd.h>
 #include <limits.h>
 #define READ_FAIL -1
-#define LINEFEED '\n'
 
 char	*get_next_line(int fd)
 {
 	int			read_status;
-	char		buffer[BUFFER_SIZE];
+	char		buffer;
 	char		*curr_line;
+	char		*temp;
 	static char	*next_line;
 
 	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0)
 		return (NULL);
-	
+	curr_line = NULL;
+	buffer = gnl_strcalloc(BUFFER_SIZE);
+	if (read(fd, buffer, BUFFER_SIZE) == READ_FAIL)
+		return (NULL);
+	temp = gnl_strcat(next_line, buffer)
 }
--- GetNextLine/get_next_line_utils.c
+++ GetNextLine/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 12:57:01 by dkim2             #+#    #+#             */
-/*   Updated: 2021/12/02 19:25:32 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/03 07:23:21 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,20 @@
 int	gnl_strlen(char *str)
 {
 	if (!str)
-		return (-1);
+		return (0);
 	if (*str)
 		return (gnl_strlen(str + 1) + 1);
 	else
 		return (0);
 }
 
-char	*gnl_stralloc(int strlen)
+char	*gnl_strcalloc(int strlen)
 {
 	char	*new_str;
 	int		i;
 
+	if (strlen < 0)
+		return (NULL);
 	new_str = malloc(sizeof(char) * (strlen + 1));
 	if (!new_str)
 		return (NULL);
@@ -36,8 +38,33 @@ char	*gnl_stralloc(int strlen)
 	return (new_str);
 }
 
+char	*gnl_strrealloc(char *str, int size)
+{
+	char	*new;
+	int		i;
+	int		len;
+
+	if (size < 0)
+		return (NULL);
+	if (!str)
+		return (gnl_strcalloc(0));
+	len = gnl_strlen(str);
+	if (size < len)
+		len = size;
+	new = gnl_strcalloc(size);
+	if (!new)
+		return (NULL);
+	i = -1;
+	while (++i < len)
+		new[i] = str[i];
+	free(str);
+	return (new);
+}
+
 char	*gnl_strchr(char *str, char c)
 {
+	if (!str)
+		return (NULL);
 	while (*str && *str != c)
 		str++;
 	if (*str == c)
@@ -46,19 +73,40 @@ char	*gnl_strchr(char *str, char c)
 		return (NULL);
 }
 
-char	*gnl_strndup(char *str, size_t n)
+char	*gnl_substr(char const *s, unsigned int start, size_t len)
 {
+	char	*substr;
 	size_t	i;
-	char	*new_str;
+	size_t	size;
 
-	new_str = gnl_stralloc(n + 1);
-	if (new_str == NULL)
+	if (!s)
 		return (NULL);
+	size = gnl_strlen((char *)s);
+	substr = gnl_strcalloc(len);
+	if (!substr)
+		return (NULL);	
 	i = 0;
-	while (i < n && str[i])
+	while (i < len && start + i < size)
 	{
-		new_str[i] = str[i];
+		substr[i] = s[start + i];
 		i++;
 	}
-	return (new_str);
+	return (substr);
+}
+
+char	*gnl_strappend(char *front, char *end)
+{
+	int		f_size;
+	int		e_size;
+	int		i;
+
+	f_size = gnl_strlen(front);
+	e_size = gnl_strlen(end);
+	front = gnl_strrealloc(front, f_size + e_size);
+	if (!front)
+		return (NULL);
+	i = -1;
+	while (++i < e_size)
+		front[f_size + i] = end[i];
+	return (front);
 }
