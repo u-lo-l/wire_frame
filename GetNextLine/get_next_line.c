e3cd378ede8654cbe0d9f1096ebe42b351631756
dkim2 <jeffk0408@naver.com>
2021-12-04 20:49:28 +0900

    mid save

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 12:56:54 by dkim2             #+#    #+#             */
-/*   Updated: 2021/12/04 18:56:18 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/04 20:49:09 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <unistd.h>
 #include <limits.h>
+#include <stdio.h>
 
 char	*gnl_read(int fd, char *save_line)
 {
@@ -22,10 +23,15 @@ char	*gnl_read(int fd, char *save_line)
 	buffer = gnl_strcalloc(BUFFER_SIZE);
 	if (!buffer)
 		return (NULL);
-	read_status = read(fd, buffer, BUFFER_SIZE);
-	if (read_status < 1)
-		return (NULL);
-	save_line = gnl_strappend(save_line, buffer);
+	while (gnl_strchr(save_line, '\n') == NULL)
+	{
+		read_status = read(fd, buffer, BUFFER_SIZE);
+		if (read_status == -1)
+			break ;
+		save_line = gnl_strappend(save_line, buffer);
+		if (save_line == NULL)
+			break ;
+	}
 	free(buffer);
 	return (save_line);
 }
@@ -33,7 +39,7 @@ char	*gnl_read(int fd, char *save_line)
 char	*get_next_line(int fd)
 {
 	int			read_status;
-	char		buffer;
+	char		*buffer;
 	char		*curr_line;
 	char		*temp;
 	static char	*next_line;
@@ -41,20 +47,41 @@ char	*get_next_line(int fd)
 	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = gnl_strcalloc(BUFFER_SIZE);
+	if (!buffer)
+		return (NULL);
 	read_status = read(fd, buffer, BUFFER_SIZE);
 	next_line = gnl_strappend(next_line, buffer);
 	free(buffer);
-	temp = gnl_strchr(next_line, LINEFEED);
+	temp = gnl_strchr(next_line, '\n');
 	if (temp != NULL)
 	{
 		curr_line = gnl_substr(next_line, 0, temp + 1 - next_line);
 		temp = gnl_substr(temp + 1, 0, gnl_strlen(temp + 1));
-		if (!next_line)
-			free(next_line);
+		free(next_line);
 		next_line = temp;
 		return (curr_line);
 	}
-	if (read_status == READ_FAIL)
+	if (read_status == -1)
 		return (next_line);
-	get_next_line(fd);
+	return (get_next_line(fd));
+}
+
+#include <fcntl.h>
+int main()
+{
+	int fd = open("text", O_RDONLY);
+	char	*s;
+	int i = 0;
+	while (i++ < 10)
+	{
+		s = get_next_line(fd);
+		if (!s)
+		{
+			printf("(null)\n");
+			break ;
+		}
+		printf(">\n%s>\n", s);
+		free(s);
+	}
+	close(fd);
 }
--- GetNextLine/get_next_line.h
+++ GetNextLine/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 18:29:31 by dkim2             #+#    #+#             */
-/*   Updated: 2021/12/04 18:34:16 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/04 20:30:33 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 3
 # endif
 # include<unistd.h>
 # include<stdlib.h>
@@ -21,7 +21,7 @@ int		gnl_strlen(char *str);
 char	*gnl_strcalloc(int len);
 char	*gnl_strreallo(char *str, int size);
 char	*gnl_strchr(char *str, char c);
-char	*gnl_substr(char *str, int start, int size);
+char	*gnl_substr(char const *str, unsigned int start, size_t size);
 char	*gnl_strappend(char *front, char *end);
 char	*get_next_line(int fd);
 #endif
