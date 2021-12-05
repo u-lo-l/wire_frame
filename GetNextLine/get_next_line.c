ece17ddceea2e6dfa7dd3d427b0144fd19c4a2ed
U-lo-l <jeffk0408@naver.com>
2021-12-06 02:04:57 +0900

    for test

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,14 +6,13 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 12:56:54 by dkim2             #+#    #+#             */
-/*   Updated: 2021/12/05 23:20:53 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/06 02:04:46 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <unistd.h>
 #include <limits.h>
-#include <stdio.h>
 
 char	*gnl_strappend(char *front, char *end)
 {
@@ -34,73 +33,48 @@ char	*gnl_strappend(char *front, char *end)
 	return (front);
 }
 
-char	*get_next_line(int fd)
+char	*temp_f(char **next_line);
 {
 	int			read_status;
 	char		*temp;
 	char		*curr_line;
-	static char	*next_line;
-
+	
 	curr_line = NULL;
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
 	temp = gnl_strcalloc(BUFFER_SIZE);
 	if (!temp)
 		return (NULL);
 	read_status = read(fd, temp, BUFFER_SIZE);
-	next_line = gnl_strappend(next_line, temp);
+	*next_line = gnl_strappend(*next_line, temp);
 	free(temp);
-	temp = gnl_strchr(next_line, '\n');
+	temp = gnl_strchr(*next_line, '\n');
 	if (temp != NULL)
 	{
-		curr_line = gnl_substr(next_line, 0, temp + 1 - next_line);
+		curr_line = gnl_substr(*next_line, 0, temp + 1 - *next_line);
 		temp = gnl_substr(temp + 1, 0, gnl_strlen(temp + 1));
-		free(next_line);
+		free(*next_line);
 		next_line = gnl_substr(temp, 0, gnl_strlen(temp));
 		free(temp);
-		return (curr_line);
 	}
 	else if (read_status < 1)
 	{
 		if (gnl_strlen(next_line))
-			curr_line = gnl_substr(next_line, 0, gnl_strlen(next_line));
-		free(next_line);
+			curr_line = gnl_substr(*next_line, 0, gnl_strlen(*next_line));
+		free(*next_line);
 		next_line = NULL;
-		return (curr_line);
 	}
-	return (get_next_line(fd));
-}
-/*====================================*/
-/*
-void	ppp(char *str)
-{
-	if (str)
-		printf("%s",str);
-	else
-		printf("null\n");
+	return (curr_line);
 }
 
-#include <fcntl.h>
-int main()
+char	*get_next_line(int fd)
 {
-	int fd = open("text", O_RDONLY);
-	char	*s;
-	// while ((s = get_next_line(fd)))
-	// {
-	// 	printf("%s", s);
-	// 	free(s);
-	// }
-	int i = 0;
-	while (++i < 10)
-	{
-		s = get_next_line(fd);
-		if (s)
-			printf("==========\n%s==========\n",s);
-		else
-			printf("null\n");
-		printf("POINTER : %p\n\n", s);
-		if (s)
-			free(s);
-	}
-	close(fd);
-}*/
+	char		*curr_line;
+	static char	*next_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	curr_line = temp_f(&next_line);
+	if (curr_line)
+		return (curr_line);
+	else
+		return (get_next_line(fd));
+}
