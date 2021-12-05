be86367bf5407105e85fea66e63df41f028c30cb
U-lo-l <jeffk0408@naver.com>
2021-12-06 02:07:08 +0900

    for test

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 12:56:54 by dkim2             #+#    #+#             */
-/*   Updated: 2021/12/06 02:06:22 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/06 02:07:04 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ char	*gnl_strappend(char *front, char *end)
 	return (front);
 }
 
-char	*temp_f(char **next_line);
+char	*temp_f(char **next_line)
 {
 	int		read_status;
 	char	*temp;
