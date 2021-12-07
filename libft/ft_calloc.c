dee76b8b375f404720c839afdd2d598797f7881e
u-lo-l <jeffk0408@naver.com>
2021-12-07 20:27:21 +0900

    real final

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 15:21:36 by dkim2             #+#    #+#             */
-/*   Updated: 2021/12/07 17:14:58 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/07 20:26:06 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,13 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*temp;
 
 	temp = malloc(size * count);
-	if (!temp)
-		return (NULL);
-	ft_bzero(temp, count * size);
+	if (size * count == 0)
+		((char *)temp)[0] = 0;
+	else
+	{
+		if (!temp)
+			return (NULL);
+		ft_bzero(temp, count * size);
+	}
 	return (temp);
 }
--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dkim2 <dkim2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 13:44:38 by dkim2             #+#    #+#             */
-/*   Updated: 2021/11/27 02:00:25 by dkim2            ###   ########.fr       */
+/*   Updated: 2021/12/07 20:23:56 by dkim2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	suffix_len;
 	char	*new_str;
 
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
 	prefix_len = ft_strlen(s1);
 	suffix_len = ft_strlen(s2);
 	new_str = malloc(sizeof(char) * (prefix_len + suffix_len + 1));
 	if (!new_str)
 		return (NULL);
-	i = 0;
-	while (i < prefix_len)
-	{
+	i = -1;
+	while (++i < prefix_len)
 		new_str[i] = s1[i];
-		i++;
-	}
-	i = 0;
-	while (i < suffix_len)
-	{
+	i = -1;
+	while (++i < suffix_len)
 		new_str[prefix_len + i] = s2[i];
-		i++;
-	}
 	new_str[prefix_len + i] = 0;
 	return (new_str);
 }
