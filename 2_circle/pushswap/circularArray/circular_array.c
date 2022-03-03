34666997d2afa3b1af429d7bd0141921922b5d71
ulol <jeffk0408@naver.com>
2022-03-03 17:17:44 +0900

    circulararray files classified

---
+++ 2_circle/pushswap/circularArray/circular_array.c
@@ -0,0 +1,58 @@
+#include "circular_array.h"
+#include <stdlib.h>
+t_mystack	*new_stack(int size)
+{
+	t_mystack	*stack;
+
+	stack = malloc(sizeof(t_mystack));
+	if (!stack)
+		return (NULL);
+	stack->max_size = size;
+	stack->curr_element_count = 0;
+	stack->top_index = 0;
+	stack->bottom_index = 0;
+	stack->array = malloc(sizeof(int) * size);
+	if (!(stack->array))
+	{
+		free(stack);
+		return (NULL);
+	}
+	return (stack);
+}
+
+void	delete_stack(t_mystack *stack)
+{
+	free(stack->array);
+	free(stack);
+}
+
+int	convert_index(int idx, int max_size)
+{
+	idx += max_size;
+	idx %= max_size;
+	return (idx);
+}
+
+/*
+int	traverse(t_mystack *stack, (int)*func(int, int))
+{
+	int	i;
+	int	top_i;
+	int	bot_i;
+	int res;
+
+	if (!stack)
+		return (ERROR);
+	i = top_i;
+	while (TRUE)
+	{
+		if (i == bot_i)
+			break ;
+		res = func(stack->array[i], stack->array[convert_index(i - 1)]);
+		if (res == ERROR)
+			return (ERROR);
+		i = convert_index(--i);
+	}
+	return (res);
+}
+*/
---
+++ 2_circle/pushswap/circularArray/circular_array_check.c
@@ -0,0 +1,84 @@
+#include "circular_array.h"
+
+int	is_ascending(t_mystack *stack)
+{
+	int	curr;
+	int	next;
+	int	bot_i;
+
+	if (!stack)
+		return (ERROR);
+	curr = stack->top_index;
+	bot_i = stack->bottom_index;
+	while (TRUE)
+	{
+		if (curr == bot_i)
+			return (TRUE);
+		next = convert_index(curr - 1, stack->max_size);
+		if (stack->array[curr] > stack->array[next])
+			return (FALSE);
+		curr = convert_index(--curr, stack->max_size);
+	}
+}
+
+int	is_descending(t_mystack *stack)
+{
+	int	curr;
+	int	next;
+	int	bot_i;
+
+	if (!stack)
+		return (ERROR);
+	curr = stack->top_index;
+	bot_i = stack->bottom_index;
+	while (TRUE)
+	{
+		if (curr == bot_i)
+			return (TRUE);
+		next = convert_index(curr - 1, stack->max_size);
+		if (stack->array[curr] < stack->array[next])
+			return (FALSE);
+		curr = convert_index(--curr, stack->max_size);
+	}
+}
+
+int	not_duplicated(t_mystack *stack, int key)
+{
+	int	curr;
+	int	bot_i;
+
+	if (!stack)
+		return (ERROR);
+	curr = stack->top_index;
+	bot_i = stack->bottom_index;
+	while (curr != bot_i)
+	{
+		if (stack->array[curr] == key)
+			return (FALSE);
+		curr = convert_index(--curr, stack->max_size);
+	}
+	return (TRUE);
+}
+/*
+int	get_fst_pivot(t_mystack *stack)
+{
+	int total_size;
+	int curr;
+	int bot_i
+	int ret;
+	int count;
+
+	total_size = stack->curr_element_count;
+	curr = stack->top_index;
+	bot_i = stack->bottom_index;
+	count = 0;
+	ret = stack->array[curr];
+	while (count < total_size / 3)
+	{
+		if ()
+		curr = convert_index(--curr);
+	}
+}
+
+int	get_snd_pivot(t_mystack *stack);
+*/
