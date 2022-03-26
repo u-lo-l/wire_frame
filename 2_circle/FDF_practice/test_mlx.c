aca6f3bf0a563c352095b9b8bcd530c9592c04af
u-lo-l <jeffk0408@naver.com>
2022-03-27 00:55:43 +0900

    fdf day1

--- 2_circle/FDF_practice/test_mlx.c
+++ 2_circle/FDF_practice/test_mlx.c
@@ -3,10 +3,14 @@
 int main(void)
 {
 	void	*mlx;
-	void	*mlx_win;
+	void	*img;
+	// void	*mlx_win;
 
 	mlx = mlx_init();
+	/*
 	if(!(mlx_win = mlx_new_window(mlx, 800, 450, "HI!")))
 		return 0;
-	mlx_loop(mlx);
+	mlx_loop(mlx);*/
+	if (!(img = mlx_new_image(mlx, 800, 450)))
+		return 0;
 }
